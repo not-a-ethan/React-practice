61c78294219f573eb01aaefa5bb6a988fd57b3a4
hackclub-ethan <195668068+hackclub-ethan@users.noreply.github.com>
2025-06-19 11:49:10 -0400

    Set up the router

---
+++ src/react/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <a href="./">Home</a> | <a href="./domains">What is a domain?</a> | <a href="./reactive">About Reactive</a>
+        </footer>
+    )
+}
+
+export default Footer;
