8454351eab551f980444fbad31af8397fe82e48d
hackclub-ethan <195668068+hackclub-ethan@users.noreply.github.com>
2025-06-22 16:57:58 -0400

    Made some more pages

---
+++ src/react/nested.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Nested() {
+    return (
+        <>
+            <p>Nested</p>
+        </>
+    )
+}
+
+export default Nested;
---
+++ src/react/reactive.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Reactive() {
+    return (
+        <>
+            <h1>Reactive</h1>
+        </>
+    )
+}
+
+export default Reactive;
