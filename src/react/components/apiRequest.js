855b381bbea83f9bf59a6bd69851b34fc743dfc7
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-06-23 15:14:29 -0400

    Tried to make it so a random todo is fetched but did not work

--- src/react/components/apiRequest.js
+++ src/react/components/apiRequest.js
@@ -5,12 +5,12 @@ import DisplayObj from "./displayObj";
 
 function Request() {
     const [data, setData] = useState();
-    
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
+
+    fetch(`https://jsonplaceholder.typicode.com/todos/1`)
         .then(response => response.json())
         .then(json => setData(json));
 
-    return data ? <DisplayObj obj={data} />: "";
+    return data ? <DisplayObj obj={data} /> : "";
 }
 
 export default Request;
